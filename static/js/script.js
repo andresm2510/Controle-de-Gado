0276e9589b57d966ede2c5b929a9c87ece08798a
André Silva Mendes <andre.smendes.20231@poli.ufrj.br>
2023-10-17 17:07:13 -0300

    cabou

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,14 @@
 let subMenu = document.getElementById("subMenu");
 
-        function toggleMenu() {
-            subMenu.classList.toggle("open-menu");
-        }
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+}
 
 let read_button = document.querySelectorAll('.read_button');
 
-for(let i = 0; i<read_button.length; i++){
-    read_button[i].addEventListener('click', function(){
+for (let i = 0; i < read_button.length; i++) {
+    read_button[i].addEventListener('click', function () {
         read_button[i].parentNode.classList.toggle('active')
     })
 }
+
